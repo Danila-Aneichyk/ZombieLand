3c8aa374a34f92939326b484f41512e6281a3a9e
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-08-27 20:12:02 +0300

    Add player and player movement.

---
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+namespace ZombieLand.Game.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        #region Variables
+
+        [SerializeField] private float _speed = 4f;
+        private Transform _cachedTransform;
+        private Camera _mainCamera;
+
+        #endregion
+
+
+        #region Unity lifecycle
+
+        private void Awake()
+        {
+            _cachedTransform = transform;
+            _mainCamera = Camera.main; 
+        }
+
+        private void Update()
+        {
+            Move();
+            Rotate();
+        }
+
+
+        #region Private methods
+
+        private void Move()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector2 direction = new Vector2(horizontal, vertical);
+            Vector3 moveDelta = direction * (_speed * Time.deltaTime);
+            _cachedTransform.position += moveDelta;
+        }
+
+        private void Rotate()
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Vector3 worldPoint = _mainCamera.ScreenToWorldPoint(mousePosition);
+            worldPoint.z = 0f;
+
+            Vector3 direction = worldPoint - transform.position;
+            _cachedTransform.up = direction;
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
